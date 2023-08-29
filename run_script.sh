997995f3404fdecc44b9c28e5721ae2106c9f624
jianpengz <jianpengznpu@gmail.com>
2023-08-29 22:59:10 +0800

    update TransDoD

--- run_script.sh
+++ run_script.sh
@@ -1,10 +1,10 @@
 cd TransDoD/
 
 # Training
-CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nproc_per_node=2 --master_port=$RANDOM train.py \
+CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 train.py \
 --train_list='MOTS/MOTS_train.txt' \
 --snapshot_dir='snapshots/TransDoDNet/' \
---nnUNet_preprocessed='/media/userdisk2/jpzhang/nnU_data_SSD/nnUNet_preprocessed' \
+--nnUNet_preprocessed='/path/to/nnUNet_preprocessed' \
 --input_size='64,192,192' \
 --learning_rate=2e-4 \
 --batch_size=2 \
@@ -13,10 +13,10 @@ CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nproc_per_node=2 -
 
 # Testing
 cd TransDoD/
-CUDA_VISIBLE_DEVICES=6 python test.py \
+CUDA_VISIBLE_DEVICES=0 python test.py \
 --val_list='MOTS/MOTS_test.txt' \
---nnUNet_preprocessed='/media/userdisk2/jpzhang/nnU_data_SSD/nnUNet_preprocessed' \
---reload_path='/home/jpzhang/faster1/jpzhang/myproject-Seg/MOTS-pro2/f_transformers/snapshots_a/TrDoD_e3d3_level3_mem2_dynhead_3_8_c192_seed123_v1/checkpoint.pth' \
+--nnUNet_preprocessed='/path/to/nnUNet_preprocessed' \
+--reload_path='/path/to/checkpoint.pth' \
 --reload_from_checkpoint=True \
 --save_path='outputs/TransDoDNet' \
 --input_size='64,192,192' 
